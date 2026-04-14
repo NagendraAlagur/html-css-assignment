bab34687931291880a938745a640a46318132c58
NagendraAlagur <nagendra@suhanshtech.com>
2026-04-15 02:09:18 +0530

    practice about router and navigation

--- angular/myApplication/src/app/app.routes.ts
+++ angular/myApplication/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { Admin } from './components/admin/admin';
+import { ControlFlow } from './components/control-flow/control-flow';
+import { DataBinding } from './components/data-binding/data-binding';
+import { SignalEx } from './components/signal-ex/signal-ex';
+import { User } from './components/user/user';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{
+  path:'Admin',
+  component:Admin
+},
+{
+  path:'control-flow',
+  component:ControlFlow
+},
+{
+  path:'data-binding',
+  component:DataBinding
+},
+{
+  path:'signal-ex', 
+  component:SignalEx
+},
+{
+  path:'user',
+  component:User
+}
+];
